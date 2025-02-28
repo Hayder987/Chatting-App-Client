57b6e690eb0194672646ee2338bbfe7732a56e58
Hayder Ali <hayderbd4290@gmail.com>
2025-02-28 13:52:55 +0600

    custom hook created for get message

--- src/hooks/useGetMessages.jsx
+++ src/hooks/useGetMessages.jsx
@@ -1,9 +1,39 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import useConversation from "../zustand/useConversation"
+import toast from "react-hot-toast"
+import axios from "axios"
 
 
 const useGetMessages = () => {
   const [loading, setLoading] = useState(false)
-  const {messages, setMessages}
+  const {messages, setMessages, selectedConversation} = useConversation()
+
+  useEffect(()=>{
+   const getMessages = async ()=>{
+    setLoading(true)
+    try{
+      const {data} = axios.get(`/api/messages/${selectedConversation._id}`)
+
+      if(data.error) {
+        throw new Error(data.error)
+      }
+      setMessages(data)
+    }
+    catch(err){
+     toast.error(err.message)
+    }
+    finally{
+     setLoading(false)
+    }
+   }
+
+   if(selectedConversation?._id) getMessages()
+
+   
+
+  }, [selectedConversation?._id, setMessages])
+  
+  return {messages, loading}
 }
 
 export default useGetMessages
