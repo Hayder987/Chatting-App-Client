f6a18a7198d00f947f4e359af72d2864e08d9232
Hayder <hayderbd4290@gmail.com>
2025-03-04 19:12:16 +0600

    update host port

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   server: {
-    port: 3000,
+    host: '0.0.0.0', 
+    port: 3000,       
     proxy: {
-      "/api": {
-        target: "https://chatting-app-server-n9zq.onrender.com",
+      '/api': {
+        target: 'https://chatting-app-server-n9zq.onrender.com',
         changeOrigin: true,  
-        secure: false, 
+        secure: false,
       },
     },
     cors: {
-      origin: "*", 
-      methods: ["GET", "POST", "PUT", "DELETE"], 
-      allowedHeaders: ["Content-Type", "Authorization"],
+      origin: '*',                      
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     },
   },
 })
