465554fdc0a43a47d0a5b929602dc3229341e8cf
Hayder <hayderbd4290@gmail.com>
2025-03-04 18:48:29 +0600

    Fixed extractTime import issue

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,6 @@
-import { extractTime } from "../../../backend/utils/extractTime";
+// import { extractTime } from "../../../backend/utils/extractTime";
 import useAuthContext from "../hooks/useAuthContext"
+import { extractTime } from "../utils/extractTime";
 import useConversation from "../zustand/useConversation";
 
 
@@ -39,23 +40,3 @@ export default Message
 
 
 
-// const Message = () => {
-//   return (
-//     <div className="chat chat-end">
-//         <div className="chat-image avatar">
-//            <div className="w-10 rounded-full">
-//             <img 
-//             src="https://i.ibb.co.com/yQVc3zX/280112897-1476710909415001-2943046122797346934-n.jpg" 
-//             alt="Chat user Image" 
-//             className="" 
-//             />
-
-//            </div>
-//         </div>
-//         <div className="chat-bubble text-white bg-blue-500">Hi! Orin</div>
-//         <div className="chat-footer text-xs text-gray-400 flex gap-1 items-center">12:10</div>
-//     </div>
-//   )
-// }
-
-// export default Message
